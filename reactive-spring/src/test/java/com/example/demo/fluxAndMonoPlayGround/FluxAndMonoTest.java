28623cf81aa0666d5d3eecbf97313e2de065986b
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-19 14:00:44 +0530

    Added a Test Case

--- reactive-spring/src/test/java/com/example/demo/fluxAndMonoPlayGround/FluxAndMonoTest.java
+++ reactive-spring/src/test/java/com/example/demo/fluxAndMonoPlayGround/FluxAndMonoTest.java
@@ -3,19 +3,35 @@ package com.example.demo.fluxAndMonoPlayGround;
 import org.junit.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 	
+//	@Test
+//	public void fluxTest() {
+//		
+//		Flux<String> stringFlux = Flux.just("Spring" ,"Spring Boot", "Reactive Spring")
+//									  //.concatWith(Flux.error(new RuntimeException("Exception Occured")))
+//									  .concatWith(Flux.just("After Error"))
+//									  .log();
+//		
+//		stringFlux.subscribe(System.out::println,
+//				(e) -> System.err.println(e),
+//				() -> System.out.println("Completed"));
+//		
+//	}
+	 
 	@Test
-	public void fluxTest() {
-		
+	public void fluxTestElements_WithoutErrors() {
 		Flux<String> stringFlux = Flux.just("Spring" ,"Spring Boot", "Reactive Spring")
-									  //.concatWith(Flux.error(new RuntimeException("Exception Occured")))
-									  .concatWith(Flux.just("After Error"));
+									.log();
 		
-		stringFlux.subscribe(System.out::println,
-				(e) -> System.err.println(e),
-				() -> System.out.println("Completed"));
+		StepVerifier.create(stringFlux)
+					.expectNext("Spring")
+					.expectNext("Spring Boot")
+					.expectNext("Reactive Spring")
+					.verifyComplete();
+		
 		
 		
 		
