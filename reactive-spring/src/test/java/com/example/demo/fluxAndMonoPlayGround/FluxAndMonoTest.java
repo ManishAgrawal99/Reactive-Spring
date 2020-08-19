17ccab145b8dc173aa11a1ab3d49f6737a9727ae
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-19 13:34:42 +0530

    Adding Exception to FLux and Handling it

--- reactive-spring/src/test/java/com/example/demo/fluxAndMonoPlayGround/FluxAndMonoTest.java
+++ reactive-spring/src/test/java/com/example/demo/fluxAndMonoPlayGround/FluxAndMonoTest.java
@@ -9,9 +9,11 @@ public class FluxAndMonoTest {
 	@Test
 	public void fluxTest() {
 		
-		Flux<String> stringFlux = Flux.just("Spring" ,"Spring Boot", "Reactive Spring");
+		Flux<String> stringFlux = Flux.just("Spring" ,"Spring Boot", "Reactive Spring")
+									  .concatWith(Flux.error(new RuntimeException("Exception Occured")));
 		
-		stringFlux.subscribe(System.out::println);
+		stringFlux.subscribe(System.out::println,
+				(e) -> System.err.println(e));
 		
 		
 		
