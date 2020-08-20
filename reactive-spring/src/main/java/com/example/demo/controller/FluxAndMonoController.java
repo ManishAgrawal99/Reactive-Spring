b42f5ad58d0e264bcae5b37bfe639d8dd5985ef8
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-20 12:39:52 +0530

    Sending data as Flux

---
+++ reactive-spring/src/main/java/com/example/demo/controller/FluxAndMonoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class FluxAndMonoController {
+	
+	@GetMapping("/flux")
+	public Flux<Integer> returnFlux(){
+		 return Flux.just(1,2,3,4)
+				 	.log();
+	}
+}
