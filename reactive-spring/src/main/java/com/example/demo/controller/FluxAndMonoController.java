e9ce1beb44ea9b688de94d5446f0ffd82628221f
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-20 13:23:58 +0530

    Sending data as a stream from controller

--- reactive-spring/src/main/java/com/example/demo/controller/FluxAndMonoController.java
+++ reactive-spring/src/main/java/com/example/demo/controller/FluxAndMonoController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,14 @@ public class FluxAndMonoController {
 	@GetMapping("/flux")
 	public Flux<Integer> returnFlux(){
 		 return Flux.just(1,2,3,4)
+				 	.delayElements(Duration.ofSeconds(1))
+				 	.log();
+	}
+	
+	@GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	public Flux<Integer> returnFluxStream(){
+		 return Flux.just(1,2,3,4)
+				 	.delayElements(Duration.ofSeconds(1))
 				 	.log();
 	}
 }
