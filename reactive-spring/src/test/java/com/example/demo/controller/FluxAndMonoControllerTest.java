77cd13f1f19f9997b93d771cc2867cd3a3cbada5
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-21 12:16:06 +0530

    Added Test Cases for Reactive Controller

--- reactive-spring/src/test/java/com/example/demo/controller/FluxAndMonoControllerTest.java
+++ reactive-spring/src/test/java/com/example/demo/controller/FluxAndMonoControllerTest.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 //import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import reactor.core.publisher.Flux;
@@ -38,6 +44,61 @@ public class FluxAndMonoControllerTest {
 					.expectNext(4)
 					.verifyComplete();
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void flux_approach2() {
+		
+		
+		webTestClient.get().uri("/flux")
+					.accept(MediaType.APPLICATION_JSON_UTF8)
+					.exchange()
+					.expectStatus().isOk()
+					.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+					.expectBodyList(Integer.class)
+					.hasSize(4);
+		
+		
+	}
+	
+	
+	@Test
+	public void flux_approach3() {
+		
+		
+		List<Integer> expectedIntegerList = Arrays.asList(1,2,3,4);
+
+        @SuppressWarnings("deprecation")
+		EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient
+                .get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertEquals(expectedIntegerList,entityExchangeResult.getResponseBody());
+		
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+    public void flux_approach4(){
+
+        List<Integer> expectedIntegerList = Arrays.asList(1,2,3,4);
+
+         webTestClient
+                .get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith((response) -> {
+                    assertEquals(expectedIntegerList, response.getResponseBody());
+                });
+
+    }
 	
 	
 }
